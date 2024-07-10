53b81c4d274d2953d2ad3bfa62d0925dbc8c82a5
Jenisa Merlin D <102733648+Jenisa-Merlin@users.noreply.github.com>
2024-07-10 15:22:47 +0530

    backend server connected

--- web/scripts/patch/api.js
+++ web/scripts/patch/api.js
@@ -1,121 +1,400 @@
-import { app } from "../app.js";
-class ComfyApi extends EventTarget {
-  #registered = new Set();
-  getUserConfig = async () => ({ storage: "browser", migrated: true });
+// class ComfyApi extends EventTarget {
+//   #registered = new Set();
 
-  constructor() {
-    super();
-    this.api_host = location.host;
-    this.api_base = location.pathname.split("/").slice(0, -1).join("/");
-    this.initialClientId = sessionStorage.getItem("clientId");
-  }
+//   constructor() {
+//     super();
+//     this.api_host = location.host;
+//     this.api_base = location.pathname.split("/").slice(0, -1).join("/");
+//     this.initialClientId = sessionStorage.getItem("clientId");
+//   }
 
-  apiURL(route) {
-    return this.api_base + route;
-  }
+//   apiURL(route) {
+//     if (route.startsWith('/view')) {
+//       return "https://b2a6225d81e01.notebooksc.jarvislabs.net/" + route;
+//     }
+//     return this.api_base + route;
+//   }
 
-  fetchApi(route, options) {
-    if (!options) {
-      options = {};
-    }
-    if (!options.headers) {
-      options.headers = {};
-    }
-    options.headers["Comfy-User"] = this.user;
-    return fetch(this.apiURL(route), options);
-  }
+//   fetchApi(route, options) {
+//     if (!options) {
+//       options = {};
+//     }
+//     if (!options.headers) {
+//       options.headers = {};
+//     }
+//     options.headers["Comfy-User"] = this.user;
+//     return fetch(this.apiURL(route), options);
+//   }
 
-  addEventListener(type, callback, options) {
-    super.addEventListener(type, callback, options);
-    this.#registered.add(type);
-  }
+//   addEventListener(type, callback, options) {
+//     super.addEventListener(type, callback, options);
+//     this.#registered.add(type); 
+//   }
 
-  async getUserData(file, options) {
-    return this.fetchApi(`/userdata/${encodeURIComponent(file)}`, options);
-  }
+//   #pollQueue() {
+//     setInterval(async () => {
+//       try{
+//         const resp = await this.fetchApi("https://2c9ab42a769f1.notebooksb.jarvislabs.net/prompt");
+//         const status = await resp.json();
+//         this.dispatchEvent(new CustomEvent("status", {detail: status}));
+//       } catch(error){
+//         this.dispatchEvent(new CustomEvent("status", {detail: null}));
+//       }
+//     }, 1000);
+//   }
 
-  async listUserData(dir, recurse, split) {
-    const resp = await this.fetchApi(
-      `/userdata?${new URLSearchParams({
-        recurse,
-        dir,
-        split,
-      })}`
-    );
-    if (resp.status === 404) return [];
-    if (resp.status !== 200) {
-      throw new Error(
-        `Error getting user data list '${dir}': ${resp.status} ${resp.statusText}`
-      );
-    }
-    return resp.json();
-  }
+//   createSocket(isReconnect) {
+// 		if (this.socket) {
+// 			return;
+// 		}
 
-  async getSystemStats() {}
+// 		let opened = false;
+// 		let existingSession = window.name;
+// 		if (existingSession) {
+// 			existingSession = "?clientId=" + existingSession;
+// 		}
+//     this.socket = new WebSocket("wss://b2a6225d81e01.notebooksc.jarvislabs.net//ws");
+// 		this.socket.binaryType = "arraybuffer";
 
-  async getSettings() {
-    return (await this.fetchApi("/settings")).json();
-  }
+// 		this.socket.addEventListener("open", () => {
+// 			opened = true;
+// 			if (isReconnect) {
+// 				this.dispatchEvent(new CustomEvent("reconnected"));
+// 			}
+// 		});
 
-  init() {
-    console.log("init");
+// 		this.socket.addEventListener("error", () => {
+// 			if (this.socket) this.socket.close();
+// 			if (!isReconnect && !opened) {
+// 				this.#pollQueue();
+// 			}
+// 		});
 
-    window.addEventListener(
-      "message",
-      (event) => {
-        if (event.data === "run") {
-          console.log("Run command received from parent");
-          performRunAction()
-            .then((data) => {
-              event.source.postMessage(
-                {
-                  type: "result",
-                  data: data,
-                },
-                event.origin
-              );
-            })
-            .catch((error) => {
-              event.source.postMessage(
-                {
-                  type: "error",
-                  message: error.message,
-                },
-                event.origin
-              );
+// 		this.socket.addEventListener("close", () => {
+// 			setTimeout(() => {
+// 				this.socket = null;
+// 				this.createSocket(true);
+// 			}, 300);
+// 			if (opened) {
+// 				this.dispatchEvent(new CustomEvent("status", { detail: null }));
+// 				this.dispatchEvent(new CustomEvent("reconnecting"));
+// 			}
+// 		});
 
-              console.log("err", error);
-            });
-        }
-      },
-      false
-    );
-    function performRunAction() {
-      return new Promise((resolve, reject) => {
-        app
-          .graphToPrompt()
-          .then((p) => {
-            const json = JSON.stringify(p.output, null, 2);
-            console.log(json);
-            resolve({ message: json });
-          })
-          .catch((error) => {
-            console.log("cat", error);
-            reject(error);
-          });
-      });
-    }
-  }
+// 		this.socket.addEventListener("message", (event) => {
+// 			try {
+//         console.log("Entered the message event listener")
+// 				if (event.data instanceof ArrayBuffer) {
+//           console.log("if condition of message event listener")
+// 					const view = new DataView(event.data);
+// 					const eventType = view.getUint32(0);
+// 					const buffer = event.data.slice(4);
+// 					switch (eventType) {
+// 					case 1:
+// 						const view2 = new DataView(event.data);
+// 						const imageType = view2.getUint32(0)
+// 						let imageMime
+// 						switch (imageType) {
+// 							case 1:
+// 							default:
+// 								imageMime = "image/jpeg";
+// 								break;
+// 							case 2:
+// 								imageMime = "image/png"
+// 						}
+// 						const imageBlob = new Blob([buffer.slice(4)], { type: imageMime });
+// 						this.dispatchEvent(new CustomEvent("b_preview", { detail: imageBlob }));
+// 						break;
+// 					default:
+// 						throw new Error(`Unknown binary websocket message of type ${eventType}`);
+// 					}
+// 				}
+// 				else {
+//             console.log("Entered the else condition of message event listener");
+// 				    const msg = JSON.parse(event.data);
+// 				    switch (msg.type) {
+// 					    case "status":
+// 						    if (msg.data.sid) {
+// 							    this.clientId = msg.data.sid;
+// 							    window.name = this.clientId; // use window name so it isnt reused when duplicating tabs
+// 								sessionStorage.setItem("clientId", this.clientId); // store in session storage so duplicate tab can load correct workflow
+// 						    }
+// 						    this.dispatchEvent(new CustomEvent("status", { detail: msg.data.status }));
+// 						    break;
+// 					    case "progress":
+// 						    this.dispatchEvent(new CustomEvent("progress", { detail: msg.data }));
+// 						    break;
+// 					    case "executing":
+// 						    this.dispatchEvent(new CustomEvent("executing", { detail: msg.data.node }));
+// 						    break;
+// 					    case "executed":
+// 						    this.dispatchEvent(new CustomEvent("executed", { detail: msg.data }));
+// 						    break;
+// 					    case "execution_start":
+// 						    this.dispatchEvent(new CustomEvent("execution_start", { detail: msg.data }));
+// 						    break;
+// 					    case "execution_error":
+// 						    this.dispatchEvent(new CustomEvent("execution_error", { detail: msg.data }));
+// 						    break;
+// 					    case "execution_cached":
+// 						    this.dispatchEvent(new CustomEvent("execution_cached", { detail: msg.data }));
+// 						    break;
+// 					    default:
+// 						    if (this.#registered.has(msg.type)) {
+// 							    this.dispatchEvent(new CustomEvent(msg.type, { detail: msg.data }));
+// 						    } else {
+// 							    throw new Error(`Unknown message type ${msg.type}`);
+// 						    }
+// 				    }
+// 				}
+// 			} catch (error) {
+// 				console.warn("Unhandled message:", event.data, error);
+// 			}
+// 		});
+// 	}
 
-  async getExtensions() {
-    const resp = await this.fetchApi("/extensions", { cache: "no-store" });
-    return await resp.json();
-  }
+//   init() {
+//     window.addEventListener(
+//       "message",
+//       (event) => {
+//         if (event.data === "run") {
+//           console.log("Run command received from parent");
+//           this.performRunAction()
+//             .then((data) => {
+//               event.source.postMessage(
+//                 {
+//                   type: "result",
+//                   data: data,
+//                 },
+//                 event.origin
+//               );
+//             })
+//             .catch((error) => {
+//               event.source.postMessage(
+//                 {
+//                   type: "error",
+//                   message: error.message,
+//                 },
+//                 event.origin
+//               );
 
-  async getNodeDefs() {
-    const resp = await this.fetchApi("/object_info", { cache: "no-store" });
-    return await resp.json();
-  }
-}
+//               console.error("Error during 'run' command:", error);
+//             });
+//         }
+//       },
+//       false
+//     );
+//   }
 
-export const api = new ComfyApi();
+//   performRunAction() {
+//     return new Promise((resolve, reject) => {
+//       app
+//         .queuePrompt()
+        // .then((p) => {
+        //   const json = JSON.stringify(p.output, null, 2);
+        //   console.log("Generated prompt:", json);
+        //   resolve({ message: json });
+        // })
+//         .catch((error) => {
+//           console.error("Error generating prompt:", error);
+//           reject(error);
+//         });
+//     });
+//   }
+
+//   async getExtensions() {
+//     const resp = await this.fetchApi("/extensions", { cache: "no-store" });
+//     return await resp.json();
+//   }
+
+//   async getEmbeddings() {
+//     const resp = await this.fetchApi("/embeddings", { cache: "no-store" });
+//     return await resp.json();
+//   }
+
+//   async getNodeDefs() {
+//     const resp = await this.fetchApi("/object_info", { cache: "no-store" });
+//     return await resp.json();
+//   }
+
+//   async queuePrompt(number, { output, workflow }) {
+//     const body = {
+//       client_id: this.clientId,
+//       prompt: output,
+//       extra_data: { extra_pnginfo: { workflow } },
+//     };
+
+//     if (number === -1) {
+//       body.front = true;
+//     } else if (number !== 0) {
+//       body.number = number;
+//     }
+
+//     const res = await this.fetchApi("https://b2a6225d81e01.notebooksc.jarvislabs.net/prompt", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(body),
+//       mode: "no-cors",
+//     });
+
+//     if (!res.ok) {
+//       const errorData = await res.json();
+//       throw new Error(`Failed to queue prompt: ${errorData.message}`);
+//     }
+
+//     return res.json();
+//   }
+
+//   async getItems(type) {
+// 		if (type === "queue") {
+// 			return this.getQueue();
+// 		}
+// 		return this.getHistory();
+// 	}
+
+//   async getSystemStats() {}
+
+//   getUserConfig = async () => ({ storage: "browser", migrated: true });
+
+//   async getUserData(file, options) {
+//     return this.fetchApi(`/userdata/${encodeURIComponent(file)}`, options);
+//   }
+
+//   async listUserData(dir, recurse, split) {
+//     const resp = await this.fetchApi(
+//       `/userdata?${new URLSearchParams({
+//         recurse,
+//         dir,
+//         split,
+//       })}`
+//     );
+//     if (resp.status === 404) return [];
+//     if (resp.status !== 200) {
+//       throw new Error(`Error getting user data list '${dir}': ${resp.status} ${resp.statusText}`);
+//     }
+//     return resp.json();
+//   }
+
+//   async getSettings() {
+//     return (await this.fetchApi("/settings")).json();
+//   }
+// }
+
+// export const api = new ComfyApi();
+
+
+  // initWebSocket(isReconnect) {
+  //   if(this.ws){
+  //     return;
+  //   }
+  //   let opened = false;
+  //   this.ws = new WebSocket("wss://2c9ab42a769f1.notebooksb.jarvislabs.net//ws");
+  //   this.ws.binaryType = "arraybuffer";
+
+  //   this.ws.addEventListener("open", () => {
+  //     console.log("Web socket opened");
+  //     opened = true;
+  //     if(isReconnect){
+  //       this.dispatchEvent(new CustomEvent("reconnected"));
+  //     }
+  //   })
+
+  //   this.ws.addEventListener("error", () => {
+  //     if(this.ws) {
+  //       this.ws.close();
+  //     }
+  //     if(!isReconnect && !opened){
+  //       this.#pollQueue();
+  //     }
+  //   })
+
+  //   this.ws.addEventListener("close", () => {
+  //     setTimeout(() => {
+  //       this.ws = null;
+  //       this.initWebSocket(true);
+  //     }, 300);
+  //     if(opened){
+  //       this.dispatchEvent(new CustomEvent("status", {detail: null}))
+  //       this.dispatchEvent(new CustomEvent("reconnecting"));
+  //     }
+  //   });
+
+  //   this.ws.addEventListener("message", (event) => {
+  //         console.log(event.data);
+  //         try {
+  //           if (event.data instanceof ArrayBuffer) {
+  //             const view = new DataView(event.data);
+  //             const eventType = view.getUint32(0);
+  //             const buffer = event.data.slice(4);
+  //             switch (eventType) {
+  //             case 1:
+  //               const view2 = new DataView(event.data);
+  //               const imageType = view2.getUint32(0)
+  //               let imageMime
+  //               switch (imageType) {
+  //                 case 1:
+  //                 default:
+  //                   imageMime = "image/jpeg";
+  //                   break;
+  //                 case 2:
+  //                   imageMime = "image/png"
+  //               }
+  //               const imageBlob = new Blob([buffer.slice(4)], { type: imageMime });
+  //               this.dispatchEvent(new CustomEvent("b_preview", { detail: imageBlob }));
+  //               break;
+  //             default:
+  //               throw new Error(`Unknown binary websocket message of type ${eventType}`);
+  //             }
+  //           }
+            // else {
+            //     const msg = JSON.parse(event.data);
+            //     switch (msg.type) {
+            //       case "status":
+            //         if (msg.data.sid) {
+            //           this.clientId = msg.data.sid;
+            //           window.name = this.clientId; // use window name so it isnt reused when duplicating tabs
+            //         sessionStorage.setItem("clientId", this.clientId); // store in session storage so duplicate tab can load correct workflow
+            //         }
+            //         this.dispatchEvent(new CustomEvent("status", { detail: msg.data.status }));
+            //         break;
+            //       case "progress":
+            //         this.dispatchEvent(new CustomEvent("progress", { detail: msg.data }));
+            //         break;
+            //       case "executing":
+            //         this.dispatchEvent(new CustomEvent("executing", { detail: msg.data.node }));
+            //         break;
+            //       case "executed":
+            //         this.dispatchEvent(new CustomEvent("executed", { detail: msg.data }));
+            //         break;
+            //       case "execution_start":
+            //         this.dispatchEvent(new CustomEvent("execution_start", { detail: msg.data }));
+            //         break;
+            //       case "execution_error":
+            //         this.dispatchEvent(new CustomEvent("execution_error", { detail: msg.data }));
+            //         break;
+            //       case "execution_cached":
+            //         this.dispatchEvent(new CustomEvent("execution_cached", { detail: msg.data }));
+            //         break;
+            //       default:
+            //         if (this.#registered.has(msg.type)) {
+            //           this.dispatchEvent(new CustomEvent(msg.type, { detail: msg.data }));
+            //         } else {
+            //           throw new Error(`Unknown message type ${msg.type}`);
+            //         }
+            //     }
+            // }
+  //         } catch (error) {
+  //           console.warn("Unhandled message:", event.data, error);
+  //         }
+  //   });
+  // }
+
+  // sendMessage(message) {
+  //   if (this.ws && this.ws.readyState === WebSocket.OPEN) {
+  //     this.ws.send(message);
+  //   } else {
+  //     console.error("WebSocket is not open.");
+  //   }
+  // }
--- x_main.py
+++ x_main.py
@@ -1,19 +1,19 @@
-from aiohttp import web
-import aiohttp
 import os
 import json
 import glob
-import nodes
 import urllib
+from aiohttp import web
+import aiohttp
+import node
+import websockets
+import asyncio
 
 routes = web.RouteTableDef()
-web_root = os.path.join(os.path.dirname(
-            os.path.realpath(__file__)), "web")
+web_root = os.path.join(os.path.dirname(os.path.realpath(__file__)), "web")
 
 @routes.get("/")
 async def get_root(request):
     return web.FileResponse(os.path.join(web_root, "index.html"))
-
 
 @routes.get("/users")
 async def get_users(request):
@@ -21,15 +21,12 @@ async def get_users(request):
 
 @routes.get("/extensions")
 async def get_extensions(request):
-    files = glob.glob(os.path.join(
-        glob.escape(web_root), 'extensions/**/*.js'), recursive=True)
-    
+    files = glob.glob(os.path.join(glob.escape(web_root), 'extensions/**/*.js'), recursive=True)
     extensions = list(map(lambda f: "/" + os.path.relpath(f, web_root).replace("\\", "/"), files))
-    
-    for name, dir in nodes.EXTENSION_WEB_DIRS.items():
+
+    for name, dir in node.EXTENSION_WEB_DIRS.items():
         files = glob.glob(os.path.join(glob.escape(dir), '**/*.js'), recursive=True)
-        extensions.extend(list(map(lambda f: "/extensions/" + urllib.parse.quote(
-            name) + "/" + os.path.relpath(f, dir).replace("\\", "/"), files)))
+        extensions.extend(list(map(lambda f: "/extensions/" + urllib.parse.quote(name) + "/" + os.path.relpath(f, dir).replace("\\", "/"), files)))
 
     return web.json_response(extensions)
 
@@ -42,12 +39,21 @@ async def fetch_data(url):
                 return data
             else:
                 return {'error': f'Failed to fetch data, status code: {response.status}'}
-        
+
 @routes.get("/object_info")
 async def main(request):
     url = 'https://raw.githubusercontent.com/Kiruthika-V-G/comfyui-nodes/main/object_info.json'  # Replace with your endpoint
     data = await fetch_data(url)
     return web.json_response(data)
+
+async def websocket_to_cloud():
+    uri = "wss://b2a6225d81e01.notebooksc.jarvislabs.net//ws"
+    async with websockets.connect(uri) as websocket:
+        while True:
+            data = await websocket.recv()
+            print(f"Received from cloud: {data}")
+            await websocket.send("Message from local server")
+
 
 def create_app():
     app = web.Application()
@@ -57,5 +63,6 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    web.run_app(app, port=8080)
-    
+    loop = asyncio.get_event_loop()
+    loop.create_task(websocket_to_cloud())
+    web.run_app(app, host="localhost", port=8080)
