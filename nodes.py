5eeae3f1d823e3f072896d6c72185e3c84373739
Talmaj <Talmaj@users.noreply.github.com>
2026-04-30 01:30:08 +0200

    Cogvideox (#13402)
    ---------
    
    Co-authored-by: kijai <40791699+kijai@users.noreply.github.com>
    Co-authored-by: Talmaj Marinc <talmaj@comfy.org>

--- nodes.py
+++ nodes.py
@@ -2463,7 +2463,7 @@ async def init_builtin_extra_nodes():
         "nodes_curve.py",
         "nodes_rtdetr.py",
         "nodes_frame_interpolation.py",
-        "nodes_sam3.py"
+        "nodes_sam3.py",
     ]
 
     import_failed = []
